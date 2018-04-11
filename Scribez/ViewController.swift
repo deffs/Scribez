ac42dea7f168bb8ec4500056f33b30c00bae8950
Alex de France <defrance@umich.edu>
2018-04-11 00:08:00 -0400

    Initial Commit

---
+++ Scribez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribez
+//
+//  Created by Alex de France on 4/11/18.
+//  Copyright © 2018 Def Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
